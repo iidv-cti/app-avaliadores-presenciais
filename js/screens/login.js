38ec3e928312f3db662ad3d6fae5826bc30b9096
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2019-11-03 16:15:17 -0300

    Ok]

---
+++ js/screens/login.js
@@ -0,0 +1,23 @@
+$(function(){
+    $("[data-cpf]").mask("000.000.000-00", {reverse: 0});
+    
+    $("#prog").css("display","none");
+
+    $("#form").submit(function(e){
+        e.preventDefault();
+
+        IIDVuserLogin(
+            $("#cpf").val(),
+            $("#password").val(),
+            function(user) {
+                $("#prog").css("display","none");
+                window.location = "painel.html";
+            }, function(error) {
+                $("#prog").css("display","none");
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                alert(error.message);
+            });
+    });
+});
